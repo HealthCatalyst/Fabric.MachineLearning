670ba2fcf62bb6ea0d3c83537d04cd2ed31c3f0b
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-03-31 12:12:04 -0700

    add R to docker
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN yum -y update; yum clean all
 RUN yum -y install epel-release; yum clean all
 RUN yum -y install nodejs npm; yum clean all
 
+RUN yum -y install R; yum clean all
+
+RUN R --version
 ADD . /src
 
 RUN cd /src; npm install
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 8080;
 
+var R = require("r-script");
+
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
 
-app.get('/listUsers', function(req, res) {
+app.get('/jobs', function(req, res) {
     res.end("hello");
 });
